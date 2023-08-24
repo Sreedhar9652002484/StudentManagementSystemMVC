19cf9c8b25a283dd2a4da2ba23453ce868040679
Sreedhar9652002484 <nagallisreedhar1918.com>
2023-08-24 12:23:32 +0530

    [SREEDHAR] Add: Added Student Record Management Using MVC

---
+++ CRUDMVC/Models/Student.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Numerics;
+
+public class Student
+{
+    public int Id { set; get; }
+   [Required]
+    public string FirstName { set; get; }
+    [Required]
+    public string LastName { set; get; }
+    [Required]
+    public string Email { set; get; }
+    [Required]
+    public string Mobile { set; get; }
+    public string Address { set; get; }
+}
---
+++ CRUDMVC/Utility/ConnectionString.cs
@@ -0,0 +1,12 @@
+﻿namespace StudentRecordManagementSystem.Utility
+{
+    public class ConnectionString
+    {
+        private static string cName = "Data Source=(localdb)\\MSSQLLocalDB; Initial Catalog=StudentManagement;Integrated Security=True;";
+
+        public static string CName
+        {
+            get => cName;
+        }
+    }
+}
